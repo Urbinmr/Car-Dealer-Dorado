2aa67511e2b6e9c6e65d43ddcc3c39b84f38b13c
Matthew <murbina@us.ci.org>
2021-06-24 16:44:33 -0400

    add react window

--- app/src/components/VehicleList.js
+++ app/src/components/VehicleList.js
@@ -1,36 +1,53 @@
-import { CarContext, FilterContext } from "../App";
-import { useContext, useEffect, useState } from "react";
-import VehicleDetail from "./VehicleDetail";
+import {CarContext, FilterContext} from "../App"
+import {useContext, useEffect, useState, useCallback} from "react"
+import VehicleDetail from "./VehicleDetail"
+import {FixedSizeList} from 'react-window'
 
 function VehicleList(props) {
-  const { cars } = useContext(CarContext);
-  const { filters } = useContext(FilterContext);
-  const [visibleCars, setVisibleCars] = useState(cars);
+  const {cars} = useContext(CarContext)
+  const {filters} = useContext(FilterContext)
+  const [visibleCars, setVisibleCars] = useState(cars)
 
   useEffect(() => {
-    let tempCars = cars.filter((car) => car.available);
+    let tempCars = cars.filter((car) => car.available)
 
     tempCars = tempCars.filter(
       (car) => car.make === filters.make || filters.make === "All"
-    );
+    )
     tempCars = tempCars.filter(
       (car) => car.model === filters.model || filters.model === "All"
-    );
+    )
     tempCars = tempCars.filter(
       (car) => car.year === filters.year || filters.year === "All"
-    );
-    setVisibleCars(tempCars);
-  }, [filters, cars]);
+    )
+    setVisibleCars(tempCars)
+  }, [filters, cars])
 
-  let carItems = visibleCars.map((car) => {
-    return <VehicleDetail key={car.id} value={car} type="list" />;
-  });
+  // let carItems = visibleCars.map((car) => {
+  //   return <VehicleDetail key={car.id} value={car} type="list"/>
+  // })
+
+  const Row = useCallback(({index, style}) => {
+    const vehicle = visibleCars[index]
+    return (
+      <div style={style}>
+        <VehicleDetail key={vehicle.id} value={vehicle} type="list" {...vehicle} />
+      </div>
+    )
+  }, [visibleCars])
 
   return (
     <div className="car-list" aria-label="cars">
-      {carItems}
+      <FixedSizeList
+        height={window.innerHeight - 100}
+        width={window.innerWidth - 50}
+        itemSize={250}
+        itemCount={visibleCars.length}
+      >
+        {Row}
+      </FixedSizeList>
     </div>
-  );
+  )
 }
 
-export default VehicleList;
+export default VehicleList
